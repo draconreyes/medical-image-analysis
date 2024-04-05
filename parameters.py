cd9dfccc95ebab5cf48959fcc8c28616d95b1d98
Diego  Reyes <diego.reyes@signa.com.co>
2024-04-04 19:09:22 -0500

    fix: validation of acquisitions files

--- parameters.py
+++ parameters.py
@@ -16,35 +16,57 @@ for folder_name in os.listdir(os.path.join(current_dir, base_dir)):
     try:
         folder_path = os.path.join(current_dir, base_dir, folder_name)
         print(f"Calculando carpeta:{folder_path}")
-        file_path = os.path.join(folder_path, 'acquisition_1\deformable\correctedForPVE\htv1_4.nii')
-        imagen_1 = nib.load(file_path)
-        acquisition_1 = imagen_1.get_fdata()
-        file_path = os.path.join(folder_path, 'acquisition_2\deformable\correctedForPVE\htv1_4.nii')
-        imagen_2 = nib.load(file_path)
-        acquisition_2 = imagen_2.get_fdata()
-        file_path = os.path.join(folder_path, 'acquisition_3\deformable\correctedForPVE\htv1_4.nii')
-        imagen_3 = nib.load(file_path)
-        acquisition_3 = imagen_3.get_fdata()
-        union_acqui_1_vs_acqui_2 = 0
-        union_acqui_1_vs_acqui_3 = 0
-        for i in range(acquisition_1.shape[0]):
-            for j in range(acquisition_1.shape[1]):
-                for k in range(acquisition_1.shape[2]):
-                    if acquisition_1[i, j, k] == acquisition_2[i, j, k]:
-                        union_acqui_1_vs_acqui_2 +=1
-                    elif acquisition_1[i, j, k] == acquisition_3[i, j, k]:
-                        union_acqui_1_vs_acqui_3 +=1
-        results.append(
-            {
-                "Folder":folder_name,
-                "DSC_acquisition_1_vs_acquisition_2":((2*union_acqui_1_vs_acqui_2)/(acquisition_1.size + acquisition_2.size)),
-                "DSC_acquisition_1_vs_acquisition_3":((2*union_acqui_1_vs_acqui_3)/(acquisition_1.size + acquisition_3.size)),
-                "SENS_acquisition_1_vs_acquisition_2":((union_acqui_1_vs_acqui_2)/((union_acqui_1_vs_acqui_2+(acquisition_1.size/acquisition_2.size)))),
-                "SENS_acquisition_1_vs_acquisition_3":((union_acqui_1_vs_acqui_3)/((union_acqui_1_vs_acqui_3+(acquisition_1.size/acquisition_3.size)))),
-                "PPV_acquisition_1_vs_acquisition_2":((union_acqui_1_vs_acqui_2)/(acquisition_2.size)),
-                "PPV_acquisition_1_vs_acquisition_3":((union_acqui_1_vs_acqui_3)/(acquisition_3.size))
-            }
-        )
+        file_path_acquisition_1 = os.path.join(folder_path, 'acquisition_1\deformable\correctedForPVE\htv1_4.nii')
+        if os.path.exists(file_path_acquisition_1):
+            imagen_1 = nib.load(file_path_acquisition_1)
+            acquisition_1 = imagen_1.get_fdata()
+            temporal_result = {}
+            acquisition_2_exists = False
+            acquisition_3_exists = False
+            acquisition_2_exists
+            file_path_acquisition_2 = os.path.join(folder_path, 'acquisition_2\deformable\correctedForPVE\htv1_4.nii')
+            if os.path.exists(file_path_acquisition_2):
+                imagen_2 = nib.load(file_path_acquisition_2)
+                acquisition_2 = imagen_2.get_fdata()
+                acquisition_2_exists = True
+            file_path_acquisition_3 = os.path.join(folder_path, 'acquisition_3\deformable\correctedForPVE\htv1_4.nii')
+            if os.path.exists(file_path_acquisition_3):
+                imagen_3 = nib.load(file_path_acquisition_3)
+                acquisition_3 = imagen_3.get_fdata()
+                acquisition_3_exists = True
+            union_acqui_1_vs_acqui_2 = 0
+            union_acqui_1_vs_acqui_3 = 0
+            for i in range(acquisition_1.shape[0]):
+                for j in range(acquisition_1.shape[1]):
+                    for k in range(acquisition_1.shape[2]):
+                        if  acquisition_2_exists and acquisition_1[i, j, k] == acquisition_2[i, j, k]:
+                            union_acqui_1_vs_acqui_2 +=1
+                        elif acquisition_3_exists and acquisition_1[i, j, k] == acquisition_3[i, j, k]:
+                            union_acqui_1_vs_acqui_3 +=1
+            results.append(
+                {
+                    "Folder":folder_name,
+                    "DSC_acquisition_1_vs_acquisition_2":((2*union_acqui_1_vs_acqui_2)/(acquisition_1.size + acquisition_2.size)) if acquisition_2_exists else "acquisition_2 no existe",
+                    "DSC_acquisition_1_vs_acquisition_3":((2*union_acqui_1_vs_acqui_3)/(acquisition_1.size + acquisition_3.size)) if acquisition_3_exists else "acquisition_3 no existe",
+                    "SENS_acquisition_1_vs_acquisition_2":((union_acqui_1_vs_acqui_2)/((union_acqui_1_vs_acqui_2+(acquisition_1.size/acquisition_2.size)))) if acquisition_2_exists else "acquisition_2 no existe",
+                    "SENS_acquisition_1_vs_acquisition_3":((union_acqui_1_vs_acqui_3)/((union_acqui_1_vs_acqui_3+(acquisition_1.size/acquisition_3.size)))) if acquisition_3_exists else "acquisition_3 no existe",
+                    "PPV_acquisition_1_vs_acquisition_2":((union_acqui_1_vs_acqui_2)/(acquisition_2.size)) if acquisition_2_exists else "acquisition_2 no existe",
+                    "PPV_acquisition_1_vs_acquisition_3":((union_acqui_1_vs_acqui_3)/(acquisition_3.size)) if acquisition_3_exists else "acquisition_3 no existe"
+                }
+            )
+        else:
+            results.append(
+                {
+                    "Folder":folder_name,
+                    "DSC_acquisition_1_vs_acquisition_2": "acquisition_1 no existe",
+                    "DSC_acquisition_1_vs_acquisition_3": "acquisition_1 no existe",
+                    "SENS_acquisition_1_vs_acquisition_2": "acquisition_1 no existe",
+                    "SENS_acquisition_1_vs_acquisition_3": "acquisition_1 no existe",
+                    "PPV_acquisition_1_vs_acquisition_2": "acquisition_1 no existe",
+                    "PPV_acquisition_1_vs_acquisition_3": "acquisition_1 no existe"
+                }
+            )
+
     except Exception as ex:
         print(ex)
 
